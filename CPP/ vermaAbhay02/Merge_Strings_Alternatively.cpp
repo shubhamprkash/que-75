fbd2a8860b869e78713d88f77bdf95e8d84d6c64
Shubham Prakash <56962038+shubhamprkash@users.noreply.github.com>
2024-02-01 23:28:54 +0530

    Update and rename Merge Strings Alternatively to CPP/ vermaAbhay02/Merge_Strings_Alternatively.cpp
    update standard maintenance edit fix

--- CPP/ vermaAbhay02/Merge_Strings_Alternatively.cpp
+++ CPP/ vermaAbhay02/Merge_Strings_Alternatively.cpp
@@ -1,5 +1,5 @@
-You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
-Return the merged string.
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+// Return the merged string.
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
